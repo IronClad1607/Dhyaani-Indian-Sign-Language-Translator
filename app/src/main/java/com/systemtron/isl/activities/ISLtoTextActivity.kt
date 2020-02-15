2a8c060f62511885a0d43fac21b4565e0ea99e13
Ishaan Jain <ishaan160799@gmail.com>
2020-02-16 05:21:12 +0530

    ISL activity started

---
+++ app/src/main/java/com/systemtron/isl/activities/ISLtoTextActivity.kt
@@ -0,0 +1,15 @@
+package com.systemtron.isl.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.systemtron.isl.R
+
+class ISLtoTextActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_islto_text)
+
+
+    }
+}
--- app/src/main/java/com/systemtron/isl/fragments/HomeFragment.kt
+++ app/src/main/java/com/systemtron/isl/fragments/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.systemtron.isl.fragments
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.systemtron.isl.R
+import com.systemtron.isl.activities.ISLtoTextActivity
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
@@ -18,7 +21,18 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
+
+        btnISLtoText.setOnClickListener {
+            val intent = Intent(requireContext(),ISLtoTextActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnTexttoISL.setOnClickListener {
+
+        }
+
+
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
