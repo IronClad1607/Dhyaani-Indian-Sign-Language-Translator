ccad6ed0680cf971e748984cf22602aa0d9ebc8b
Ishaan Jain <ishaan160799@gmail.com>
2020-02-16 01:31:57 +0530

    Home Activity Started

--- app/src/main/java/com/systemtron/isl/MainActivity.kt
+++ app/src/main/java/com/systemtron/isl/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.systemtron.isl
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private var SPLASH_DELAY: Long = 5000
+
+    private var mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+            val intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        mDelayHandler = Handler()
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+    }
+
+
+    override fun onDestroy() {
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
     }
 }
