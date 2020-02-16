32a540dc1725e9a4b1ca8e55e085ac978b56ae24
Ishaan Jain <ishaan160799@gmail.com>
2020-02-16 09:07:36 +0530

    Splash Screen decided

--- app/src/main/java/com/systemtron/isl/activities/ISLtoTextActivity.kt
+++ app/src/main/java/com/systemtron/isl/activities/ISLtoTextActivity.kt
@@ -3,13 +3,14 @@ package com.systemtron.isl.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.systemtron.isl.R
+import kotlinx.android.synthetic.main.activity_islto_text.*
 
 class ISLtoTextActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_islto_text)
-
+        cameraView.setLifecycleOwner(this)
 
     }
 }
